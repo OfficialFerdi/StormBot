e36d0067462aa0fee28298e6035d1bce64d67cd6
OfficialFerdi <39998969+OfficialFerdi@users.noreply.github.com>
2018-10-13 08:43:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const fs = require("fs");
+const ms = require("ms");
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
 
